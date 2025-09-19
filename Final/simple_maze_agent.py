34111f5ecdef4a47609d589e3f54dd3c431ad73d
rv-us <rverma8@ucsc.edu>
2025-09-18 22:14:38 -0700

    Update simple_maze_agent.py

--- Final/simple_maze_agent.py
+++ Final/simple_maze_agent.py
@@ -67,8 +67,8 @@ class SimpleMazeAgent:
 
 MOVEMENT CALIBRATION (CRITICAL):
 - Speed 30 for 1 second = 30 centimeters forward
-- Speed 50 for 2.13 seconds = 90 degrees turn (right)
-- Speed 50 for 3.15 seconds = 90 degrees turn (left)
+- Speed 30 for 2.13 seconds = 90 degrees turn (right)
+- Speed 30 for 3.15 seconds = 90 degrees turn (left)
 - Speed 20 for 0.5 seconds = 15 centimeters forward
 - Speed 40 for 1.4 seconds = 60 degrees turn
 
